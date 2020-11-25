c41acccd9e61aa9b3c5f4aaa42a30143da27a797
Noise <noise087@gmail.com>
2020-11-25 13:22:35 +0400

    second

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,6 @@ const Header = () => {
     return (
         <header className='header' >
             <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1000px-Apple_logo_black.svg.png' alt='logo' />
-            header
         </header >
     )
 }
